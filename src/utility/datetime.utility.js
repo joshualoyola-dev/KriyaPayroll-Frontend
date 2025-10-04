5765e2d7fd9b214a699572027904b44c34494f43
jzaragosa06 <junjunzaragosa2309@gmail.com>
2025-10-04 11:54:07 +0800

    date/time utility function
    add function to parse dates into YYYY-MM-DD date format. add utility function to render the YYYY-MM-DD HH:MM:SS part of ISO UTC string returned by the backend:

--- src/utility/datetime.utility.js
+++ src/utility/datetime.utility.js
@@ -1,9 +1,10 @@
+//converts into ISO 18601 String format: YYYY-MM-DD format 
 export const convertToISO8601 = (date) => {
     if (!date) return null;
 
     const d = new Date(date);
 
-    if (isNaN(d)) return null; // invalid date
+    if (isNaN(d)) return null;
 
     const year = d.getFullYear();
     const month = String(d.getMonth() + 1).padStart(2, "0");
@@ -23,3 +24,21 @@ export const toSqlDateTimeString = (inputValue) => {
     if (!inputValue) return '';
     return inputValue.replace('T', ' ') + ':00';
 };
+
+//convert date object into YYYY-MM-DD HH:MM:SS
+export const convertToDatetimeString = (datetime) => {
+    if (!datetime) return null; // handle null or undefined
+
+    const date = new Date(datetime);
+
+    // Extract UTC values (no local timezone conversion)
+    const year = date.getUTCFullYear();
+    const month = String(date.getUTCMonth() + 1).padStart(2, "0");
+    const day = String(date.getUTCDate()).padStart(2, "0");
+    const hours = String(date.getUTCHours()).padStart(2, "0");
+    const minutes = String(date.getUTCMinutes()).padStart(2, "0");
+    const seconds = String(date.getUTCSeconds()).padStart(2, "0");
+
+    return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+};
+
