6094c5b984355a21c36b9ff4c3b6b7c77ee19690
jzaragosa06 <junjunzaragosa2309@gmail.com>
2025-10-06 13:29:31 +0800

    fix: fix change in api path call

--- src/services/payrun.service.js
+++ src/services/payrun.service.js
@@ -1,7 +1,7 @@
 import payroll_api from "../configs/payroll_api.config";
 
 export const getPayrun = async (company_id, payrun_id) => {
-    return await payroll_api.get(`/api/v1/payruns/${company_id}/regular/${payrun_id}`);
+    return await payroll_api.get(`/api/v1/payruns/${company_id}/${payrun_id}`);
 };
 
 export const getPayrunPayslipPayables = async (company_id, payrun_id) => {
